8cf6587501c241ed2d4577f53738e34fe41f6762
Stanislav <skonashuk@list.ru>
2022-01-22 04:28:33 +0300

    Add app routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import './App.css';
+import { Route, Routes, Navigate } from 'react-router-dom';
+import BoardPage from './Content/BoardPage/BoardPage';
 import Header from './Header/Header';
+import MainPage from './Content/MainPage/MainPage';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      Hello
+      <Routes>
+        <Route path="/trello-react" element={<Navigate to="/" />} />
+        <Route path="/" element={<MainPage />} />
+        <Route path="/board/:id" element={<BoardPage />} />
+      </Routes>
     </div>
   );
 }
