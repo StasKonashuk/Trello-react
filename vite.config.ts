d8d638c95a2de04e26c2f113b8ff70e60bfcd416
Stanislav <skonashuk@list.ru>
2022-01-22 05:48:17 +0300

    Fix vite config for deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Trello-react/',
   plugins: [tsconfigPaths(), react()],
   resolve: {
     alias: {
